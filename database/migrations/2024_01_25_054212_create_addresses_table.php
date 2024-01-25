e9c1bfd30b2f1a976902eb09a0d914a5ec3ee9a8
Ragul-itsk <ragul@itsk.in>
2024-01-25 14:48:49 +0530

    Remaining DB

---
+++ database/migrations/2024_01_25_054212_create_addresses_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->id();
+            $table->text('address')->nullable();
+            $table->unsignedBigInteger('state_id');
+            $table->unsignedBigInteger('country_id');
+            $table->unsignedBigInteger('district_id');
+            $table->string('pincode')->nullable();
+            $table->string('village_area')->nullable();
+            $table->unsignedBigInteger('address_type_id');
+            $table->morphs('addressable');
+            $table->timestamps();
+
+            $table->foreign('state_id')->references('id')->on('states');
+            $table->foreign('country_id')->references('id')->on('countries');
+            $table->foreign('district_id')->references('id')->on('districts');
+            $table->foreign('address_type_id')->references('id')->on('address_types');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('addresses');
+    }
+};
