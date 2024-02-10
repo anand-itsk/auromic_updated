f91fb53cd5e2beab7fe6756b87956b1f0c85b4b3
anand-itsk <130842437+anand-itsk@users.noreply.github.com>
2024-02-10 10:13:56 +0530

    new change

---
+++ database/migrations/2024_02_09_163712_create_product_models_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product_models', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('raw_material_id')->nullable();
+            $table->unsignedBigInteger('product_id');
+            $table->unsignedBigInteger('product_size_id')->nullable();
+            $table->string('model_code');
+            $table->string('model_name');
+            $table->string('raw_material_weight_item')->nullable();
+            $table->string('wages_product')->default(0);
+            $table->timestamps();
+            $table->foreign('raw_material_id')->references('id')->on('raw_materials')->onDelete('cascade');
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+            $table->foreign('product_size_id')->references('id')->on('product_sizes')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product_models');
+    }
+};
