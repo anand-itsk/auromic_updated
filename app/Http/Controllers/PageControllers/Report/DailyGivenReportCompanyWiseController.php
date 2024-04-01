d4805521c4eaf5a8cd280d6842f5356959ab3514
anand-itsk <130842437+anand-itsk@users.noreply.github.com>
2024-04-01 14:51:50 +0530

    updation

--- app/Http/Controllers/PageControllers/Report/DailyGivenReportCompanyWiseController.php
+++ app/Http/Controllers/PageControllers/Report/DailyGivenReportCompanyWiseController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\PageControllers\Report;
 
 use App\Http\Controllers\Controller;
 use App\Models\JobGiving;
+use App\Models\CompanyType;
+use App\Models\Company;
 use Illuminate\Http\Request;
 use Yajra\DataTables\DataTables;
 use Maatwebsite\Excel\Facades\Excel;
@@ -12,10 +14,13 @@ class DailyGivenReportCompanyWiseController extends Controller
 {
     public function index()
     {
-        return view ('pages.report.daily_given_report_cw');
+          $companyType = CompanyType::all();
+        $company = Company::all();
+        return view ('pages.report.daily_given_report_cw',compact('companyType','company'));
     }
     public function indexData()
     {
+        
         $Job_Giving = JobGiving::with('employee', 'order_details', 'delivery_chellan','product_model.productSize')->get();
         return DataTables::of($Job_Giving)->make(true);
     }
