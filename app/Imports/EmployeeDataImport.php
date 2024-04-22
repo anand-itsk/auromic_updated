89289be44c015b0ae73c1f325cc524e60140e4ee
Ragul-itsk <ragul@itsk.in>
2024-04-22 10:33:59 +0530

    correction

--- app/Imports/EmployeeDataImport.php
+++ app/Imports/EmployeeDataImport.php
@@ -2,7 +2,9 @@
 
 namespace App\Imports;
 
+use App\Models\Address;
 use App\Models\Employee;
+use DateTime;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Hash;
 use Maatwebsite\Excel\Concerns\ToCollection;
@@ -15,33 +17,70 @@ class EmployeeDataImport implements ToCollection, WithHeadingRow
      */
     public function collection(Collection $collection)
     {
-        
+
 
         foreach ($collection as $row) {
             // Check if user with the same email already exists
-            $existing = Employee::where('employee_code', $row['employee_code'])->where('employee_name', $row['employee_name'])->where('joining_date', $row['joining_date'])->where('faorhus_name', $row['faorhus_name'])->where('dob', $row['dob'])->where('gender', $row['gender'])->where('mobile', $row['mobile'])->where('email', $row['email'])->where('marital_status', $row['marital_status'])->where('std_code', $row['std_code'])->where('religion_id', $row['religion_id'])->where('prob_period', $row['prob_period'])->where('confirm_date', $row['confirm_date'])->first();
+            $existing = Employee::where('employee_code', $row['code'])
+                ->where('employee_name', $row['employee_name'])
+                ->where('joining_date', $row['date_of_joining'])
+                ->where('mobile', $row['mobile'])
+                ->where('email', $row['e_mail'])->first();
 
             if ($existing) {
                 // Skip this user or handle as needed
                 return null;
             }
-            Employee::create([
-                
-                'employee_code' => $row['employee_code'],
-                'employee_name' => $row['employee_name'],
-                'joining_date' => $row['joining_date'],
-                'faorhus_name' => $row['faorhus_name'],
-                'dob' => $row['dob'],
-                'gender' => $row['gender'],
-                'mobile' => $row['mobile'],
-                'email' => $row['email'],
-                'marital_status' => $row['marital_status'],
-                'std_code' => $row['std_code'],
-                'religion_id' => $row['religion_id'],
-                'prob_period' => $row['prob_period'],
-                'confirm_date' => $row['confirm_date'],
-                
+
+
+            if ($row['gender'] == "F") {
+                $gender = "Female";
+            } elseif ($row['gender'] == "M") {
+                $gender = "Male";
+            }
+
+            $employee = Employee::create([
+                'company_id' => $row['company_id'] ?? null,
+                'employee_code' => $row['code'] ?? null,
+                'employee_name' => $row['employee_name'] ?? null,
+                'joining_date' => isset($row['date_of_joining']) ? (new DateTime($row['date_of_joining']))->format('Y-m-d') : null,
+                'faorhus_name' => $row['fathershusbands_name'] ?? null,
+                // 'dob' => isset($row['dob']) ? (new DateTime($row['dob']))->format('Y-m-d') : null,
+                'gender' => $gender ?? null,
+                'mobile' => $row['mobile'] ?? null,
+                'email' => $row['email'] ?? null,
+                'marital_status' => $row['marital_status'] ?? null,
+                'std_code' => $row['std_code'] ?? null,
+                'religion_id' => $row['religion_id'] ?? null,
+                'prob_period' => $row['prob_period'] ?? null,
+                'confirm_date' => isset($row['confirmation_date']) ? (new DateTime($row['confirmation_date']))->format('Y-m-d') : null,
             ]);
+
+            if ($row['address_permanent'] != '') {
+
+
+                $officeAddress = new Address();
+                $officeAddress->address_type_id = 4; // Assuming this is the type ID for office addresses
+
+
+                $officeAddress->address = $row['address_permanent'];
+                $officeAddress->village_area = $row['city_permanent'];
+                $officeAddress->pincode = $row['pin_permanent'];
+                $employee->addresses()->save($officeAddress);
+            }
+
+            if ($row['address_corresp'] != '') {
+
+
+                $correspAddress = new Address();
+                $correspAddress->address_type_id = 4; // Assuming this is the type ID for office addresses
+
+
+                $correspAddress->address = $row['address_corresp'];
+                $correspAddress->village_area = $row['city_corresp'];
+                $correspAddress->pincode = $row['pin_corresp'];
+                $employee->addresses()->save($correspAddress);
+            }
         }
     }
 }
